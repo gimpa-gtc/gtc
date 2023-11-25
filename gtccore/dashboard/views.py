7365054ce6ea31f491139c4a5bba6dea6b2acaf1
DevPrinceK <princesamuelpks@gmail.com>
2023-11-25 08:52:12 +0000

    Update views.py

--- gtccore/dashboard/views.py
+++ gtccore/dashboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class DashboardView(View):
+    template = 'dashboard/dashboard.html'
+
+    def get(self, request):
+        context ={}
+        return render(request, self.template, context)
