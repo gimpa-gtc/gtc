4daf5eefe967784e9686c63707e7b67680a0043a
DevPrinceK <princesamuelpks@gmail.com>
2023-11-25 02:52:48 +0000

    Update urls.py

--- gtccore/website/urls.py
+++ gtccore/website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'website'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
 ]
