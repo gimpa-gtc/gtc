28f99d4c732716630e2167bd06e78e60b32ce281
DevPrinceK <princesamuelpks@gmail.com>
2023-11-25 23:28:23 +0000

    Update models.py

--- gtccore/dashboard/models.py
+++ gtccore/dashboard/models.py
@@ -5,6 +5,10 @@ from django.db import models
 from gtccore.library.constants import (ApplicationStatus, PaymentMode,  # noqa
                                        PaymentStatus)
 
+import random
+import string
+
+
 '''
 List of models:
 1. User
@@ -13,21 +17,43 @@ List of models:
 4. Course
 5. Course Category
 6. Payment
-7. invoice
 '''
 
 class CourseCategory(models.Model):
+    '''Course category model'''
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural='Course Categories'
 
+
+    def get_total_courses(self):
+        '''Returns the total number of courses in this category'''
+        return Course.objects.filter(category=self).count()
+    
+    def get_active_courses(self):
+        '''Returns the total number of active courses in this category'''
+        return Course.objects.filter(category=self, end_date__gte=timezone.now().date()).count() #noqa
+    
+    def get_inactive_courses(self):
+        '''Returns the total number of inactive courses in this category'''
+        return Course.objects.filter(category=self, end_date__lt=timezone.now().date()).count() #noqa
+    
+    def get_total_applications(self):
+        '''Returns the total number of applications in this category'''
+        return Application.objects.filter(course__category=self).count()
+
+    def get_total_admissions(self):
+        '''Returns the total number of admissions in this category'''
+        return Admission.objects.filter(application__course__category=self).count()
+
     def __str__(self):
         return self.name
     
 
 class Course(models.Model):
+    '''Course model'''
     title = models.CharField(max_length=200)
     description = models.TextField()
     category = models.ForeignKey(CourseCategory, on_delete=models.CASCADE)
@@ -46,15 +72,21 @@ class Course(models.Model):
     def is_active(self):
         '''Returns True if the course is active'''
         return self.end_date > timezone.now().date()
+    
+    def get_total_applications(self):
+        '''Returns the total number of applications for this course'''
+        return Application.objects.filter(course=self).count()
+    
+    def get_total_admissions(self):
+        '''Returns the total number of admissions for this course'''
+        return Admission.objects.filter(application__course=self).count()
 
     def __str__(self):
         return self.name
-    
-import random
-import string
 
 
 class Application(models.Model):
+    '''Application model'''
     def generate_application_id() -> str:
         '''Generates a unique application id'''
         return 'GTC-AP-'.join(random.choices(string.digits, k=7))
@@ -76,6 +108,30 @@ class Application(models.Model):
     application_status = models.CharField(max_length=20, default='pending', choices=APPLICATION_STATUS) #noqa
     payment_mode = models.CharField(max_length=20, default='pending', choices=PAYMENT_MODE) #noqa
     payment_status = models.CharField(max_length=20, default='pending', choices=PAYMENT_STATUS) #noqa
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Admission(models.Model):
+    '''Admission model'''
+    application = models.OneToOneField(Application, on_delete=models.CASCADE) #noqa
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Payment(models.Model):
+    '''Payment model'''
+    transaction_id = models.CharField(max_length=100)
+    application = models.OneToOneField(Application, on_delete=models.CASCADE) #noqa
+    amount = models.IntegerField(default=0)
+    network = models.CharField(max_length=5)
+    number = models.CharField(max_length=10)
+    status_code = models.CharField(max_length=20)
+    status_message = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
