298f80202df5a8ea02b88a33e3fc37e0bb9f8a69
DevPrinceK <princesamuelpks@gmail.com>
2023-11-25 23:58:14 +0000

    Update models.py

--- gtccore/dashboard/models.py
+++ gtccore/dashboard/models.py
@@ -126,3 +126,13 @@ class Payment(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Applicant(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
