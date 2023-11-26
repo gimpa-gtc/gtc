a2a9bf8ac4dc3faf6a3cca816ca6a5324246914e
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 00:54:27 +0000

    Update signals.py

--- gtccore/dashboard/signals.py
+++ gtccore/dashboard/signals.py
@@ -1,7 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import Application, Applicant
+from .models import Application, Applicant, Notification
+from gtccore.libary.services import send_sms
+from gtccore.settings import SENDER_ID, ARKESEL_API_KEY
 
 
 @receiver(post_save, sender=Application)
@@ -23,3 +25,15 @@ def extract_applicant(sender, instance, created, **kwargs):
         except Exception as er:
             print(f"Error: {er}")
         return True
+
+
+@receiver(post_save, sender=Notification)
+def notify_applicants(sender, instance, created, **kwargs):
+    '''Send the notification to applicants upon saving of notification'''
+    applicants = Applicant.objects.all()
+    msg = f"{instance.title} \n{instance.content}"
+    numbers = [str(applicant.phone) for applicant in applicants]
+    emails = [str(applicant.email) for applicant in applicants]
+
+    send_sms(SENDER_ID, msg, numbers)
+    return True
