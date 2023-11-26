746c5b99670c56c85a9fc6087cc9a699da0d5fe3
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 13:43:11 +0000

    Update admin.py

--- gtccore/dashboard/admin.py
+++ gtccore/dashboard/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Applicant)
+admin.site.register(Application)
+admin.site.register(CustomCourseRequest)
+admin.site.register(Course)
+admin.site.register(CourseCategory)
+admin.site.register(Contact)
+admin.site.register(Notification)
+admin.site.register(Faq)
+admin.site.register(Admission)
+admin.site.register(Payment)
