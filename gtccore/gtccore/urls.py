d5296dca924ac335ba67e0fe7575c23705d7eaa7
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 11:37:10 +0000

    Update urls.py

--- gtccore/gtccore/urls.py
+++ gtccore/gtccore/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('', include('accounts.urls')),
 
 ]
