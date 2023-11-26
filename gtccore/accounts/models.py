3cfb09cfe9767c14220b7ba3db33a9b0dfcf8a72
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 13:18:15 +0000

    Update models.py

--- gtccore/accounts/models.py
+++ gtccore/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from accounts.manager import AccountManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+
+    profile_pic = models.ImageField(upload_to='profile', blank=True, null=True)  # noqa
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.name if self.name else self.email # noqa
+
+    class Meta:
+        db_table = 'user'
