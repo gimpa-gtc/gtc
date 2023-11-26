99f52234153694e64f22add4f29b614254dbaf8b
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 13:43:01 +0000

    Update manager.py

--- gtccore/accounts/manager.py
+++ gtccore/accounts/manager.py
@@ -5,16 +5,16 @@ from django.contrib.auth.models import BaseUserManager
 class AccountManager(BaseUserManager):
     '''manages User account creation'''
 
-    def create_user(self, email, password, fullname='-', **kwargs):
+    def create_user(self, email, password, name='-', **kwargs):
         user = self.model(email=email, password=password,
-                          fullname=fullname, **kwargs)
+                          name=name, **kwargs)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, fullname='-', **kwargs):
+    def create_superuser(self, email, password, name='-', **kwargs):
         user = self.create_user(
-            email,  password, fullname='-', **kwargs)
+            email,  password, name='-', **kwargs)
         user.is_superuser = True
         user.is_staff = True
         user.save()
