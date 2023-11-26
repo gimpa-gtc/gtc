7376095f1f07f41ee576cc10bf0c604be750f818
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 00:15:20 +0000

    Update signals.py

--- gtccore/dashboard/signals.py
+++ gtccore/dashboard/signals.py
@@ -1,2 +1,20 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+from .models import Application, Applicant
+
+
+@receiver(post_save, sender=Application)
+def extract_applicant(sender, instance, created, **kwargs):
+    email = instance.email
+    phone = instance.phone
+
+    applicant = Applicant.objects.filter(email=email, phone=phone).first()
+    if applicant is not None:
+        pass
+    else:
+        Applicant.objects.create(
+            email=instance.email,
+            phone=instance.phone,
+            name=instance.name
+        )
