d5520c5c38c7d46dc815fc55adf0841934ee04e8
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 11:26:18 +0000

    Update models.py

--- gtccore/dashboard/models.py
+++ gtccore/dashboard/models.py
@@ -170,3 +170,15 @@ class Contact(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class CustomCourseRequest(models.Model):
+    '''Custom course request model'''
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    course = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
