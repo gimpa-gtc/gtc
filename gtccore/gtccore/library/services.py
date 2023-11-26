44280ef3480cbcf23205bae335dc2c7eef58e609
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 13:08:47 +0000

    Update services.py

--- gtccore/gtccore/library/services.py
+++ gtccore/gtccore/library/services.py
@@ -1,40 +1,60 @@
 import array
-
 import requests
-
 from fpdf import FPDF
+
+import time
 
 from gtccore import settings
 
+from fpdf import FPDF
+
+class PDF(FPDF):
+    '''Custom PDF class.'''
+    def header(self):
+        # Rendering logo:
+        logo = "gtccore\library\gimpa-round-logo.jpg"
+        self.image(logo, x=10, y=10, w=20)  # Adjust the position and size of the logo as needed
+        # Setting font: helvetica bold 15
+        self.set_font("Helvetica", "B", 15)
+        # Moving cursor to the right:
+        self.cell(80)
+        # Printing title:
+        self.cell(30, 10, "Gimpa Training & Consulting", align="C")
+        # Performing a line break:
+        self.ln(20)
+
+    def footer(self):
+        # Position cursor at 1.5 cm from bottom:
+        self.set_y(-15)
+        # Setting font: helvetica italic 8
+        self.set_font("Helvetica", "I", 8)
+        # Printing page number:
+        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
+
 def generate_admission_letter():
     # Create a PDF object
-    pdf = FPDF()
+    pdf = PDF()
 
     # Add a page
     pdf.add_page()
 
     # Set up the document
-    pdf.set_title('Admission Letter')
-
-    # Set font and size for the letterhead
     pdf.set_font('Arial', 'B', 16)
-
-    # Add the letterhead
-    pdf.cell(0, 20, 'University of Example', ln=True, align='C')
+    pdf.cell(200, 10, txt="Admission Letter", ln=True, align="C")
 
     # Set font and size for the content
     pdf.set_font('Arial', '', 12)
 
     # Add the date
-    pdf.cell(0, 10, 'Date: November 25, 2023', ln=True, align='L')
+    pdf.cell(0, 10, f"Date: {time.strftime('%d %B %Y')}", ln=True, align='L')
 
     # Add a blank line
     pdf.ln(10)
 
     # Add the recipient's address
-    pdf.multi_cell(0, 10, 'John Doe', align='L')
-    pdf.multi_cell(0, 10, '123 Main Street', align='L')
-    pdf.multi_cell(0, 10, 'Anytown, USA', align='L')
+    pdf.cell(0, 10, 'John Doe', ln=True, align='L')
+    pdf.cell(0, 10, '123 Main Street', ln=True, align='L')
+    pdf.cell(0, 10, 'Anytown, USA', ln=True, align='L')
 
     # Add a blank line
     pdf.ln(10)
@@ -46,19 +66,39 @@ def generate_admission_letter():
     pdf.ln(10)
 
     # Add the body of the letter
-    pdf.multi_cell(0, 10, 'We are pleased to inform you that you have been admitted to the University of Example for the upcoming semester.', align='L')
+    pdf.multi_cell(0, 10, 'We are pleased to inform you that you have been admitted to Gimpa Training and Consulting short professional courses.', align='L')
 
     # Add a blank line
     pdf.ln(10)
+
+    # Add program details
+    pdf.set_font('Arial', 'B', 12)
+    pdf.cell(0, 10, 'Program:', ln=True, align='L')
+    pdf.set_font('Arial', '', 12)
+    pdf.cell(0, 10, 'Course Name', ln=True, align='L')
+
+    # Add fee details
+    pdf.set_font('Arial', 'B', 12)
+    pdf.cell(0, 10, 'Fees:', ln=True, align='L')
+    pdf.set_font('Arial', '', 12)
+    pdf.cell(0, 10, 'USD 1000', ln=True, align='L')
+
+    # Add admission ID
+    pdf.set_font('Arial', 'B', 12)
+    pdf.cell(0, 10, 'Admission ID:', ln=True, align='L')
+    pdf.set_font('Arial', '', 12)
+    pdf.cell(0, 10, 'AD12345', ln=True, align='L')
+
+     # Add administrator's signature (as an image)
+    signature_image = "gtccore\library\signature.png"  # Replace with the actual path to the administrator's signature image
+    pdf.image(signature_image, x=10, y=pdf.get_y() + 10, w=30)  # Adjust the position and size of the signature image as needed
+
+    # Add a blank line
+    pdf.ln(30)
 
     # Add the closing
-    pdf.multi_cell(0, 10, 'Congratulations once again!', align='L')
+    pdf.multi_cell(0, 10, 'Sincerely, \nProf. Charles Teye Amoatey\nDirector, GTC', align='L')
 
-    # Add a blank line
-    pdf.ln(10)
-
-    # Add the signature
-    pdf.multi_cell(0, 10, 'Sincerely,', align='L')
 
     return pdf
 
