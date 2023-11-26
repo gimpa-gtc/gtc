563c89d125b9a3acd2301575c63a900aeaf7cfd8
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 13:09:12 +0000

    Update urls.py

--- gtccore/website/urls.py
+++ gtccore/website/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('contact/', views.ContactView.as_view(), name='contact'),
     path('faqs/', views.FaqsView.as_view(), name='faqs'),
+]
+
+# applications
+urlpatterns += [
     path('application/', views.ApplicationView.as_view(), name='application'),
+    path('application/status/', views.ApplicationStatusView.as_view(), name='application_status'), #noqa
+    path('application/download/', views.DownloadAdmissionLetterView.as_view(), name='download_letter'), #noqa
 ]
