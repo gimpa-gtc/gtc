66e718f8af5c1aa67312d382487e0cf721be2606
DevPrinceK <princesamuelpks@gmail.com>
2023-11-26 23:13:07 +0000

    Update applications.py

--- gtccore/dashboard/views/applications.py
+++ gtccore/dashboard/views/applications.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
-from dashboard.models import Application
+from dashboard.models import Admission, Applicant, Application
 
 
 class ApplicationsView(View):
@@ -21,5 +21,20 @@ class AdmissionsView(View):
     template = 'dashboard/pages/admissions.html'
 
     def get(self, request):
-        context ={}
+        admissions = Admission.objects.all().order_by('-created_at')
+        context ={
+            'admissions': admissions,
+        }
+        return render(request, self.template, context)
+    
+
+class ApplicantsView(View):
+    '''applicants view'''
+    template = 'dashboard/pages/applicants.html'
+
+    def get(self, request):
+        applicants = Applicant.objects.all().order_by('-created_at')
+        context ={
+            'applicants': applicants,
+        }
         return render(request, self.template, context)
