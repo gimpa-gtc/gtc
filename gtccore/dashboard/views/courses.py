d5d82e962380ee241fe9c21fbb688bf037598a87
DevPrinceK <princesamuelpks@gmail.com>
2023-11-29 15:01:07 +0000

    Update courses.py

--- gtccore/dashboard/views/courses.py
+++ gtccore/dashboard/views/courses.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
-from dashboard.models import Course
+from dashboard.models import Course, CourseCategory
 
 class CoursesView(View):
     template = 'dashboard/pages/courses.html'
@@ -10,5 +10,16 @@ class CoursesView(View):
         courses = Course.objects.all().order_by('-id')
         context ={
             'courses': courses
+        }
+        return render(request, self.template, context)
+
+class CourseCategoriesView(View):
+    '''Course categories view'''
+    template = 'dashboard/pages/course_categories.html'
+
+    def get(self, request):
+        categories = CourseCategory.objects.all().order_by('-id')
+        context ={
+            'categories': categories
         }
         return render(request, self.template, context)
