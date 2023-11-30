eefbf2372db8983bb9c9a579cb045aaaf04498ce
DevPrinceK <princesamuelpks@gmail.com>
2023-11-30 13:10:52 +0000

    Update users.py

--- gtccore/dashboard/views/users.py
+++ gtccore/dashboard/views/users.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 
+from accounts.models import User
+
 
 class UserListView(View):
     template = 'dashboard/pages/users.html'
 
     def get(self, request):
-        context ={}
+        users = User.objects.all().order_by('-created_at')
+        context ={
+            'users': users
+        }
         return render(request, self.template, context)
