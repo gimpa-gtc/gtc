9babd2f0373bb761e0e3e2921b4bb1ec5b03268f
DevPrinceK <princesamuelpks@gmail.com>
2023-12-04 11:56:36 +0000

    Update payments.py

--- gtccore/dashboard/views/payments.py
+++ gtccore/dashboard/views/payments.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.db.models import Q
 from dashboard.models import Payment
 
 
@@ -9,6 +9,14 @@ class PaymentsView(View):
 
     def get(self, request):
         payments = Payment.objects.all().order_by('-id')
+        query = request.GET.get('query')
+        if query:
+            payments = Payment.objects.filter(
+                Q(pyment__transaction_id__icontains=query) | 
+                Q(payment__phone__icontains=query) | 
+                Q(payment__status_message__icontains=query) | 
+                Q(payment__status_code__icontains=query) | 
+                Q(payment_network__icontains=query)).order_by('-id') # noqa
         context ={
             'payments': payments
         }
