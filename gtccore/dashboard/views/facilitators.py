7c0cfe19b6536cc5cdd9cd3df683a4035e675d84
DevPrinceK <princesamuelpks@gmail.com>
2023-12-04 16:18:36 +0000

    Create facilitators.py

---
+++ gtccore/dashboard/views/facilitators.py
@@ -0,0 +1,100 @@
+import csv
+from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import redirect, render
+from django.views import View
+from django.contrib import messages
+from django.db.models import Q
+from accounts.models import User
+
+
+class FacilitorsView(View):
+    template = 'dashboard/pages/users.html'
+
+    def get(self, request):
+        query = request.GET.get('query')
+        if query:
+            users = User.objects.filter(
+                Q(name__icontains=query) | 
+                Q(email__icontains=query) | 
+                Q(phone__icontains=query)).order_by('-created_at') # noqa
+        else:
+            users = User.objects.all().order_by('-created_at')
+        context ={
+            'users': users
+        }
+        return render(request, self.template, context)
+    
+
+class CreateUpdateUser(View):
+    '''Create or update user'''
+    template = 'dashboard/pages/create-update-user.html'
+
+    def get(self, request):
+        user_id = request.GET.get('user_id')
+        user = User.objects.filter(id=user_id).first()
+        context = {
+            'user': user
+        }
+        return render(request, self.template, context)
+
+    def post(self, request):
+        user_id = request.POST.get('user_id')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        role = request.POST.get('role')
+        status = request.POST.get('status')
+        try:
+            user_id = int(user_id)
+        except:
+            user_id = None
+
+        user = User.objects.filter(id=user_id).first()
+        if user:
+            # only update if user exists and attributes are not empty
+            user.name = name if name else user.name
+            user.email = email if email else user.email
+            user.phone = phone if phone else user.phone
+            user.role = role if role else user.role
+            user.status = status if status else user.status
+            user.save()
+        else:
+            user = User.objects.create(
+                name=name,
+                email=email,
+                phone=phone,
+                is_staff=role == 'staff',
+                is_superuser=role == 'superuser',
+                is_active=status == 'active'
+            )
+            messages.success(request, 'User Created Successfully')
+        redirect_url = reverse('dashboard:create_update_user') + '?user_id=' + str(user.id)
+        return redirect(redirect_url)
+
+
+    def put(self, request):
+        user_id = request.POST.get('user_id')
+        profile_pic = request.FILES.get('profile_pic')
+        user = User.objects.filter(id=user_id).first()
+        if user:
+            user.profile_pic = profile_pic
+            user.save()
+            messages.success(request, 'Profile Picture Updated Successfully')
+        else:
+            messages.error(request, 'User Not Found')
+        redirect_url = reversed('dashboard:create_update_user') + '?user_id=' + str(user_id)
+        return redirect(redirect_url)
+
+
+class DownloadUsersView(View):
+    '''Download users as csv'''
+    def get(self, request):
+        users = User.objects.all()
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="users.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['primary_key', 'name', 'email', 'phone', 'is_staff', 'is_superuser', 'is_active', 'created_at']) # noqa
+        for user in users:
+            writer.writerow([user.id, user.name, user.email, user.phone, user.is_staff, user.is_superuser, user.is_active, user.created_at])
+        return response
