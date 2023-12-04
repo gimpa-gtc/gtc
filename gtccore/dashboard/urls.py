f74313b89794d18b50d7ffc34d524f800b8a089f
DevPrinceK <princesamuelpks@gmail.com>
2023-12-04 11:37:49 +0000

    Update urls.py

--- gtccore/dashboard/urls.py
+++ gtccore/dashboard/urls.py
@@ -6,10 +6,16 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.DashboardView.as_view(), name='dashboard'),
-    path('users/', views.UserListView.as_view(), name='users'),
     path('category/', views.CategoryListView.as_view(), name='category'),
     path('settings/', views.SettingsView.as_view(), name='settings'),
     path('profile/', views.UserProfileView.as_view(), name='profile'),
+]
+
+# users
+urlpatterns += [
+    path('users/', views.UsersView.as_view(), name='users'),
+    path('create-update-user/', views.CreateUpdateUser.as_view(), name='create_update_user'), #noqa
+    path('download-users/', views.DownloadUsersView.as_view(), name='download_users'), #noqa
 ]
 
 
