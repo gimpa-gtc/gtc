84f34f786704be5801cc1dddce42c7ebffe8d202
DevPrinceK <princesamuelpks@gmail.com>
2023-12-07 21:10:26 +0000

    Update category.py

--- gtccore/dashboard/views/category.py
+++ gtccore/dashboard/views/category.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.views import View
 from django.db.models import Q
 
-from dashboard.models import CourseCategory
+from dashboard.models import Cohort, CourseCategory
 class CategoryListView(View):
     template = 'dashboard/pages/category.html'
 
@@ -22,4 +22,27 @@ class DownloadCategoriesView(View):
         writer.writerow(['name', 'created_at'])
         for category in categories:
             writer.writerow([category.name, category.created_at])
+        return response
+    
+
+class CohortsView(View):
+    template = 'dashboard/pages/cohorts.html'
+
+    def get(self, request):
+        months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+        context ={
+            'months': months
+        }
+        return render(request, self.template, context)
+    
+class DownloadCohortView(View):
+    '''Download cohorts as csv'''
+    def get(self, request):
+        cohorts = Cohort.objects.all().order_by('-id')
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="cohorts.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['name', 'start_month', 'created_at'])
+        for cohort in cohorts:
+            writer.writerow([cohort.name,cohort.start_month ,cohort.created_at])
         return response
