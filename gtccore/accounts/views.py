e5347823518a1109db9f09acfdfc5d5efacecf0f
DevPrinceK <princesamuelpks@gmail.com>
2023-12-08 09:04:35 +0000

    Update views.py

--- gtccore/accounts/views.py
+++ gtccore/accounts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.views import View
 
 
 class LoginView(View):
