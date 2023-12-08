668afea0d198f692a2ecc23293a8a681488cfa30
DevPrinceK <princesamuelpks@gmail.com>
2023-12-08 09:04:33 +0000

    Update models.py

--- gtccore/accounts/models.py
+++ gtccore/accounts/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
 from accounts.manager import AccountManager
 
 
