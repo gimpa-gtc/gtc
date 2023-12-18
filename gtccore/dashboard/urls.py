a240cfe174dc48d76c9e06c35bf7f9a83e3cba99
DevPrinceK <princesamuelpks@gmail.com>
2023-12-18 11:06:53 +0000

    Update urls.py

--- gtccore/dashboard/urls.py
+++ gtccore/dashboard/urls.py
@@ -55,11 +55,11 @@ urlpatterns += [
 ]
 
 # facilitators
-urlpatterns += [
-    path('facilitators/', views.FacilitorsView.as_view(), name='facilitators'),
-    path('download-facilitators/', views.DownloadFacilitatorsView.as_view(), name='download_facilitators'), #noqa
-    path('create-update-facilitator/', views.CreateUpdateFacilitatorView.as_view(), name='create_update_facilitator'), #noqa
-]
+# urlpatterns += [
+#     path('facilitators/', views.FacilitorsView.as_view(), name='facilitators'),
+#     path('download-facilitators/', views.DownloadFacilitatorsView.as_view(), name='download_facilitators'), #noqa
+#     path('create-update-facilitator/', views.CreateUpdateFacilitatorView.as_view(), name='create_update_facilitator'), #noqa
+# ]
 
 # payments
 urlpatterns += [
