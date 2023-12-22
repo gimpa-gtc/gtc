18ddb87ce520af86917d41784dad6fa8b6ea286d
DevPrinceK <princesamuelpks@gmail.com>
2023-12-22 18:00:18 +0000

    Update profile.py

--- gtccore/dashboard/views/profile.py
+++ gtccore/dashboard/views/profile.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.utils.decorators import method_decorator
+
+from gtccore.library.decorators import StaffLoginRequired
 
 
 class UserProfileView(View):
+    '''User profile view'''
     template = 'dashboard/pages/profile.html'
 
+    @method_decorator(StaffLoginRequired)
     def get(self, request):
         context ={}
         return render(request, self.template, context)
