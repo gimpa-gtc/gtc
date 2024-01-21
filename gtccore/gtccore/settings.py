09d0252ab3439728a46bad13ecd5ac2b4ccffa9c
princek <princesamuelpks@gmail>
2024-01-21 20:10:20 +0000

    Update settings.py

--- gtccore/gtccore/settings.py
+++ gtccore/gtccore/settings.py
@@ -111,7 +111,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles/' # For Debug True
+
+# for dreamhost
+STATIC_ROOT = '/home/dh_5p37hu/gtc.gimpa.edu.gh/public/staticfiles/'
+# dh static files wdir: /home/dh_5p37hu/gtc.gimpa.edu.gh/public/staticfiles
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
