996ade9e9f3dd82a4f3b7ad40366dfd823651ad8
princek <princesamuelpks@gmail>
2024-04-23 22:34:22 +0100

    Update teams.py

--- gtccore/dashboard/views/teams.py
+++ gtccore/dashboard/views/teams.py
@@ -1,15 +1,17 @@
 import csv
-from django.contrib.auth.mixins import PermissionRequiredMixin
+
 from django.contrib import messages
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.views import View
 from django.utils.decorators import method_decorator
+from django.views import View
 
 from dashboard.models import Facilitator
 from gtccore.library.decorators import StaffLoginRequired
+
 
 class TeamsView(PermissionRequiredMixin, View):
     template = 'dashboard/pages/teams.html'
