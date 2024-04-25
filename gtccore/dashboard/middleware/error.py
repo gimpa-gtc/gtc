7d8aab0d4f8e455e14f439300fb31160f414fc30
princek <princesamuelpks@gmail>
2024-04-25 14:27:17 +0100

    Update error.py

--- gtccore/dashboard/middleware/error.py
+++ gtccore/dashboard/middleware/error.py
@@ -12,10 +12,20 @@ class CustomErrorMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
 
+        # limit access to admin site for non-superusers
+        if request.path.startswith('/admin/') and not request.user.is_superuser:
+            return render(request, 'dashboard/403.html', status=403)
+        
+        # page not found
         if response.status_code == 404:
             return render(request, 'dashboard/404.html', status=404)
         
+        # server error
         elif response.status_code == 500:
             return render(request, 'dashboard/500.html', status=500)
+
+        # forbidden
+        elif response.status_code == 403:
+            return render(request, 'dashboard/403.html', status=403)
 
         return response
